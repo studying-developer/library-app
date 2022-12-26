15508bd27f83fa86c6c2f0ce573284b17383a686
최태현 <choi-0202@kaist.ac.kr>
2022-12-26 15:42:28 +0900

    첫 번째 커밋

---
+++ src/main/java/com/group/libraryapp/temp/PersonService.java
@@ -0,0 +1,26 @@
+package com.group.libraryapp.temp;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class PersonService {
+
+  private final AddressRepository addressRepository;
+  private final PersonRepository personRepository;
+
+  public PersonService(AddressRepository addressRepository, PersonRepository personRepository) {
+    this.addressRepository = addressRepository;
+    this.personRepository = personRepository;
+  }
+
+  @Transactional
+  public void savePerson() {
+    Person person = personRepository.save(new Person());
+    Address address = addressRepository.save(new Address());
+    person.setAddress(address);
+    address.getPerson();
+  }
+
+
+}
